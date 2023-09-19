aca738f8cf0c74a0e45e780264df6e7a47d3cbbe
ricomonster <2063313+ricomonster@users.noreply.github.com>
2023-09-19 23:15:02 +0800

    new config setup

--- nvim/lua/custom/chadrc.lua
+++ nvim/lua/custom/chadrc.lua
@@ -1,6 +1,12 @@
 ---@type ChadrcConfig 
  local M = {}
- M.ui = {theme = 'catppuccin', transparency = true}
+ M.ui = {
+  theme = 'tokyodark', 
+  transparency = true,
+  statusline = {
+    theme = 'minimal'
+  }
+}
  M.plugins = 'custom.plugins'
 
  return M
